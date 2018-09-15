0b485deadb6d6e0cf884f21e6526692f952dc4b8
Sergio640 <sergio64pacheco@gmail.com>
2018-09-15 14:35:00 -0600

    Initial commit

---
+++ app/src/main/java/com/codepath/helloworldapp/MainActivity.java
@@ -0,0 +1,71 @@
+package com.codepath.helloworldapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Sergio", "ButtonClicked");
+                ((TextView) findViewById(R.id.textView)).setTextColor(
+                        getResources().getColor(R.color.colorAccent));
+
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.rootView).setBackgroundColor(
+                        getResources().getColor(R.color.colorGray));
+            }
+        });
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Sergio", "ButtonClicked");
+                // grab text view user has inputted
+                // place the text within my text once the 'change text' button is clicked
+               String newText =  ((EditText) findViewById(R.id.editText)).getText().toString();
+
+               if (TextUtils.isEmpty(newText)) {
+                   ((TextView) findViewById(R.id.textView)).setText("Hello from Sergio!");
+               } else {
+                   ((TextView) findViewById(R.id.textView)).setText(newText);
+               }
+
+            }
+        });
+
+        findViewById(R.id.rootView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // rest text color back to blue
+                ((TextView) findViewById(R.id.textView)).setTextColor(
+                        getResources().getColor(R.color.black));
+
+                // reset the background color to
+                        findViewById(R.id.rootView).setBackgroundColor(
+                                getResources().getColor(R.color.colorPrimary));
+
+                // reset the text back to 'Hello From Sergio!'
+                ((TextView) findViewById(R.id.textView)).setText("Hello from Sergio!");
+
+        }
+
+    });
+
+}}
